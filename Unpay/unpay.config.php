a4bc3f0d953daed9027f650e200c430ad4bff36a
caokailiang <350271335@qq.com>
2016-08-11 14:09:12 +0800

    一些小修改

--- Unpay/unpay.config.php
+++ Unpay/unpay.config.php
@@ -1,4 +1,32 @@
 <?php
+//证书路径
+const SDK_SIGN_CERT_PATH='';
+//证书密码
+const SDK_SIGN_CERT_PWD='';
+//验证证书
+const SDK_VERIFY_CERT_PATH='';
+//密码加密证书
+const SDK_ENCRYPT_CERT_PATH='';
+//验证证书路径
+const SDK_VERIFY_CERT_DIR='';
+//前台通知地址
+const SDK_FRONT_NOTIFY_URL='';
+//后台异步通知地址
+const SDK_BACK_NOTIFY_URL='';
+//文件下载目录
+const SDK_FILE_DOWN_PATH='';
+//日志目录
+const SDK_LOG_FILE_PATH='';
+//日志级别
+const SDK_LOG_LEVEL='';
+//商户id(app端)
+const APP_APPID='';
+//商户id（网页端）
+const WEB_APPID='';
+//支付请求地址（app地址）
+const SDK_APP_REQUEST_URL='';
+//支付请求地址(h5的请求地址)
+const SDK_H5_REQUEST_URL='';
 return array(
 	
 );
